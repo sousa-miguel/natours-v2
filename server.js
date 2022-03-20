e0d282c07842f8641df488716cdcae0dc5f9ee4d
sousa-miguel <miiguelsousa@hotmail.com>
2022-03-20 12:21:40 +0000

    setting up ESLint with prettier in VScode

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app'); // run app code only after setting dotenv config!!
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+app.listen(port, () => console.log(`App running on port ${port}...`));
