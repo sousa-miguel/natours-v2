4cd154e1ae31d9fabd1c56dbe7171145fe578e12
sousa-miguel <miiguelsousa@hotmail.com>
2022-03-20 21:01:34 +0000

    connection to MongoDB using Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,55 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app'); // run app code only after setting dotenv config!!
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A Tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A Tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 2.9,
+  price: 51,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error 😡: ', err);
+  }); // save tour (document) to collection in the database
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App running on port ${port}...`));
