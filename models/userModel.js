d0181da7be1fae4a7e3f94d4aa8f6dd1d58d9850
sousa-miguel <miiguelsousa@hotmail.com>
2022-04-03 11:07:32 +0100

    user schema + signup route

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'User password must have more or equal than 8 characters'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // this only works on CREATE or SAVE!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run function if password was modified
+  if (!this.isModified('password')) return next();
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
