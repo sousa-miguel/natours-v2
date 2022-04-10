0fe4141f4a82b1ac6443c55d95c492f9ca6d514e
sousa-miguel <miiguelsousa@hotmail.com>
2022-04-10 17:58:39 +0100

    nested GET endpoint

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,16 +1,12 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const APIFeatures = require('../utils/apiFeatures');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  // EXECUTE QUERY
-  const features = new APIFeatures(Review.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-  const reviews = await features.query;
+  let filter;
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
